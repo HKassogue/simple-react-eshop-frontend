8e498977a7e3e4f35d5eb1a0e0ee3161a1e60699
Dr Hamidou Kassogué <hamidoukass@gmail.com>
2024-06-01 09:18:26 +0000

    Project structure

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import logo from '../logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello word</h1>
+      <img src={logo}/>
+    </div>
+  );
+}
+
+export default App;
