743a0bcaa6abdc6feab6a5665bf4a3f58c90da98
Dr Hamidou Kassogué <hamidoukass@gmail.com>
2024-06-15 09:01:48 +0000

    Use of lacalstorage to save cart with reload

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import Accueil from './Accueil';
 import Panier from './Panier';
 
 
 function App() {
-  let [panier, updatePanier] = useState([]);
+  const savedPanier = localStorage.getItem('panier');
+  const [panier, updatePanier] = useState(savedPanier ? JSON.parse(savedPanier) : []);
+	useEffect(() => {
+		localStorage.setItem('panier', JSON.stringify(panier));
+	}, [panier]);
 
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Accueil panier={panier} updatePanier={updatePanier}/>}></Route>
-        <Route path='/panier' element={<Panier panier={panier} updatePanier={updatePanier}/>}></Route>
+        <Route path='/' element={<Accueil panier={panier} updatePanier={updatePanier} />}></Route>
+        <Route path='/panier' element={<Panier panier={panier} updatePanier={updatePanier} />}></Route>
       </Routes>
     </Router>
   );
